11df121c2534f633e3ccf61f62bdb9bee513240d
Vihas30622 <2420030622@klh.edu.in>
2025-08-04 10:47:05 +0530

    OOP SKILL 01

---
+++ OOP_SKILL_01/src/OOP_SKILL_01/Code01.java
@@ -0,0 +1,27 @@
+package OOP_SKILL_01;
+
+public class Code01 {
+
+	public static void main(String[] args) {
+		
+		byte age = 18;
+        short currentYear = 2025;
+        int population = 1400000000;
+        long distanceToMoon = 384400000L;
+        float pi = 3.14f;
+        double gravity = 9.80665;
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Current Year: " + currentYear);
+        System.out.println("Country Population: " + population);
+        System.out.println("Distance to Moon (in meters): " + distanceToMoon);
+        System.out.println("Value of Pi: " + pi);
+        System.out.println("Acceleration due to Gravity: " + gravity);
+        System.out.println("Student Grade: " + grade);
+        System.out.println("Is Java fun to learn? " + isJavaFun);
+
+	}
+
+}
---
+++ OOP_SKILL_01/src/OOP_SKILL_01/Code02.java
@@ -0,0 +1,21 @@
+package OOP_SKILL_01;
+
+public class Code02 {
+public static void main(String[] args) {
+
+	        long rollNumber = 2420030622L;
+	        String name = "Vihas Vattikuti";
+	        float marks = 92.5f;
+	        char gender = 'M';
+	        boolean isPassed = true;
+
+	        System.out.println("Roll Number: " + rollNumber);
+	        System.out.println("Name: " + name);
+	        System.out.println("Marks: " + marks);
+	        System.out.println("Gender: " + gender);
+	        System.out.println("Passed: " + isPassed);
+	    }
+	}
+
+
+
